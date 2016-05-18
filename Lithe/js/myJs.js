5be370dce7f0ac56e21c6e60e6d118b26e762663
nomosoro <nomosoro@hotmail.com.hk>
2016-05-18 18:24:50 +0800

    Hovering. MixitUp() disabled.
    Implied 3 role. mix it up disabled.

--- Lithe/js/myJs.js
+++ Lithe/js/myJs.js
@@ -130,7 +130,7 @@ function set_container(){
 
 		});
 }
-
+var columns=['#column1','#column2','#column3'];
 function load_template(){
 
 	var news = [
@@ -138,21 +138,72 @@ function load_template(){
         paragraphs:"新华社北京5月17日电 （记者吴晶、华春雨）中共中央总书记、国家主席、中央军委主席习近平17日上午在京主持召开哲学社会科学工作座谈会并发表重要讲话。他强调，一个没有发达的自然科学的国家不可能走在世界前列，一个没有繁荣的哲学社会科学的国家也不可能走在世界前列。坚持和发展中国特色社会主义，哲学社会科学具有不可替代的重要地位，哲学社会科学工作者具有不可替代的重要作用。坚持和发展中国特色社会主义，必须高度重视哲学社会科学，结合中国特色社会主义伟大实践，加快构建中国特色哲学社会科学。", 
         time_dev:'22 hours ago', source_link: 'http://news.ifeng.com/a/20160517/48786384_0.shtml'
         },
-        { categories: "category-1 category-3", img_status: "img-less" ,img_link : "Resources/HK/bgImg.jpg", title : "张德江将参观香港科学园 了解创新科技情况",
+        { categories: "category-1 category-2", img_status: "img-less" ,img_link : "Resources/HK/bgImg.jpg", title : "张德江将参观香港科学园 了解创新科技情况",
         paragraphs:"香港科学园区是香港高新科技以及应用科技的一个研发基地，从2001年开始启用，目前分为三期一共有600多家的企业进驻，提供一万三千多个就业岗位，主要以创新科技企业为主。张德江此行也是要了解香港创新科技的一个发展情况，目前香港特区政府也是大力推展创新科技的发展，科学园区也是扮演着非常重要的角色。科学园区将会在今年夏天建成亚洲最大的数据信息中心，也是提供给软件的开发者以及学术研究来使用的。张德江此行也会到里面参观部分的创科企业，听取一个最新科研成果。以上是我们在现场了解到的最新情形，先把时间交还给您，亚蒨。", 
         time_dev:'30 minutes', source_link: 'http://news.ifeng.com/a/20160518/48794685_0.shtml'
         },
-        { categories: "category-1 category-3", img_status: "" ,img_link : "Resources/World/1/pics/1.jpeg", title : "Donald Trump: Ban all Muslim travel to U.S.. ",
+        { categories: "category-1 category-3", img_status: "" ,img_link : "Resources/World/2/pics/1.jpg", title : "Donald Trump: Ban all Muslim travel to U.S.. ",
         paragraphs:"Donald J. Trump is calling for a total and complete shutdown of Muslims enteri Donald J. Trump is calling for a total and complete shutdown of Muslims enteri Donald J. Trump is calling for a total and complete shutdown of Muslims enteri",
         time_dev:'2 days ago', source_link: 'http://edition.cnn.com/2015/12/07/politics/donald-trump-muslim-ban-immigration/'
+        },
+        { categories: "category-1 category-2", img_status: "" ,img_link : "Resources/HK/4/pics/1.jpg", title : "张德江抵达香港画面",
+        paragraphs:"5月17日中午，全国人大委员长张德江专机抵达香港，香港特别行政区行政长官梁振英、中央人民政府驻香港联络办公室主任张晓明与香港各界代表到机场迎接。驻香港联络办公室主任张晓明与香港各界代表到机场迎接。驻香港联络办公室主任张晓明与香港各界代表到机场迎接。驻香港联络办公室主任张晓明与香港各界代表到机场迎接。"
+        ,time_dev:'2 days ago', source_link: 'http://edition.cnn.com/2015/12/07/politics/donald-trump-muslim-ban-immigration/'
+        },
+        { categories: "category-2", img_status: "" ,img_link : "Resources/World/3/pics/1.jpg", title : "RNC chairman: Third-party run would be a 'suicide mission'",
+        paragraphs:"Donald J. Trump is calling for a total and complete shutdown of Muslims enteri Donald J. Trump is calling for a total and complete shutdown of Muslims enteri Donald J. Trump is calling for a total and complete shutdown of Muslims enteri",
+        time_dev:'2 days ago', source_link: 'http://edition.cnn.com/2015/12/07/politics/donald-trump-muslim-ban-immigration/'
+        },
+        { categories: "category-3", img_status: "" ,img_link : "Resources/World/1/pics/1.jpeg", title : "Donald Trump: Ban all Muslim travel to U.S.. ",
+        paragraphs:"They can try to hijack another party and get on the ballot, but, look,They can try to hijack another party and get on the ballot, but, look,They can try to hijack another party and get on the ballot, but, look,They can try to hijack another party and get on the ballot, but, look,",
+        time_dev:'3 days ago', source_link: 'http://edition.cnn.com/2015/12/07/politics/donald-trump-muslim-ban-immigration/'
+        },	
+        { categories: "category-3", img_status: "" ,img_link : "Resources/World/4/pics/1.jpg", title : "Donald Trump: Ban all Muslim travel to U.S.. ",
+        paragraphs:"They can try to hijack another party and get on the ballot, but, look,They can try to hijack another party and get on the ballot, but, look,They can try to hijack another party and get on the ballot, but, look,They can try to hijack another party and get on the ballot, but, look,",
+        time_dev:'3 days ago', source_link: 'http://edition.cnn.com/2015/12/07/politics/donald-trump-muslim-ban-immigration/'
         }
     ];
 
-        
+    
+    
+    
 	
-	$('#Container').append($('#block_template').tmpl(news));
+	var $template=$('#block_template');
+
+	//use 3 column template. 
+	for(i=0;i<news.length;i++)
+	{	
+		m=lowest_column(i);
+		$(columns[m]).append($template.tmpl(news[i]));
+		setTimeout(function(){}, 10)
+	}
 
 
-    alert('1');
+    
     
 }
+
+function lowest_column(x){
+	
+	var h=[];
+
+	for(j=0;j<3;j++){
+		h[j]=$(columns[j]).height();
+	}
+	
+	min=Math.min(h[0],h[1]);
+	min=Math.min(min,h[2]);
+	
+	for(j=0;j<3;j++){
+		if(min==h[j]){
+			alert(min+'   '+j);
+			return j;
+		}
+			
+	}
+		
+	
+
+	return 0;
+}
+
